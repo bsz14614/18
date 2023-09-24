a6d3a183361d4bf6463316510fdb13d60f04ee3a
bsz14614 <145478709+bsz14614@users.noreply.github.com>
2023-09-24 11:11:43 +0800

    Add files via upload

---
+++ level1-3最终版.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, c, d, e;
+	printf("请输入电梯起始楼层，和依次输入两个用户的起始楼层与目的楼层\n");
+	scanf("%d", &e);
+	scanf("%d%d", &a, &b);
+	scanf("%d%d", &c, &d);
+	int x, y;
+	x = a - e ;
+	y = c - e ;
+	if (x < 0) {
+		x = -x;
+	}
+	if (y < 0) {
+		y = -y;
+	}
+	if ((x < y) || ((x == y) && (a < c))) {
+		if ((a < c && c < b) || (a > c && c > b)) {
+			if ((c < d && d < b) || (c > d && d > b)) {
+				printf("%d %d %d %d %d\n", e, a, c, d, b);
+			} else {
+				printf("%d %d %d %d %d\n", e, a, c, b, d);
+			}
+		} else {
+			printf("%d %d %d %d %d\n", e, a, b, c, d);
+		}
+
+	} else if ((x > y) || ((x == y) && (a < c))) {
+		if ((c < a && a < d) || (c > a && a > d)) {
+			if ((a < b && b < d) || (a > b && b > d)) {
+				printf("%d %d %d %d %d\n", e, c, a, b, d);
+			} else {
+				printf("%d %d %d %d %d\n", e, c, a, d, b);
+			}
+		} else {
+			printf("%d %d %d %d %d\n", e, c, d, a, b);
+		}
+	}
+	return 0;
+}
